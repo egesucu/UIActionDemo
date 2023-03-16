4c5fb6df9de9dacdd42f17cca28363692dc04a41
Ege Sucu <iletisim@egesucu.com.tr>
2023-03-16 22:15:17 +0300

    Article related codes added

--- UIActionDemo/ViewController.swift
+++ UIActionDemo/ViewController.swift
@@ -8,10 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var actionSlider: UISlider!
+    @IBOutlet weak var actionSwitch: UISwitch!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupButtonAction()
+        setupSliderAction()
+        setupSwitchAction()
+    }
+    
+    func setupButtonAction() {
+        let buttonAction = UIAction { _ in
+            print("Button Tapped")
+        }
+        actionButton.addAction(buttonAction, for: .touchUpInside)
+    }
+    
+    func setupSliderAction() {
+        let sliderAction = UIAction { [weak self] _ in
+            guard let self else { return }
+            print("Slider value changed. New value is", Int(actionSlider.value))
+        }
+        actionSlider.addAction(sliderAction, for: .valueChanged)
+    }
+    
+    func setupSwitchAction() {
+        let switchAction = UIAction { [weak self] _ in
+            guard let self else { return }
+            print("Switch Value changed, Switch is On? ", actionSwitch.isOn)
+        }
+        actionSwitch.addAction(switchAction, for: .valueChanged)
     }
 
 
